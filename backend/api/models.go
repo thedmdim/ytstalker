00f126ae1e41895df6869e4a4f70046377d72619
Dima <dmitruk.dd@tk23.ru>
2023-11-02 10:59:59 +0400

    reactions working

--- backend/api/models.go
+++ backend/api/models.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"fmt"
-	"log"
 	"net/url"
 	"strconv"
 	"strings"
--- backend/api/server.go
+++ backend/api/server.go
@@ -9,16 +9,17 @@ import (
 	"net/http"
 	"os"
 
-	"zombiezen.com/go/sqlite/sqlitex"
-	"github.com/gorilla/mux"
 	"html/template"
+
+	"github.com/gorilla/mux"
+	"zombiezen.com/go/sqlite/sqlitex"
 )
 
 var templates = template.Must(template.ParseGlob("frontend/*/*.html"))
 
 type Server struct {
 	http.Server
-	db *sqlitex.Pool
+	db  *sqlitex.Pool
 	ytr *youtube.YouTubeRequester
 }
 
@@ -41,7 +42,7 @@ func NewServer() *Server {
 		log.Fatal("cannot create db: ", err)
 	}
 	db.Put(conn)
-	
+
 	log.Println("database ready")
 
 	// init youtube requester
@@ -49,27 +50,30 @@ func NewServer() *Server {
 
 	// init server
 	r := mux.NewRouter()
-	
+
 	server := &Server{
 		Server: http.Server{
-			Addr: config.Addr,
+			Addr:    config.Addr,
 			Handler: r,
 		},
-		db: db,
+		db:  db,
 		ytr: ytr,
 	}
 
-	r.PathPrefix("/api/videos/{video_id}/{reaction:(?:cool|trash)})").Methods("POST").HandlerFunc(server.WriteReaction).HeadersRegexp("visitor", "[0-9]{10,20}")
-	r.PathPrefix("/api/videos/random").Methods("GET").HandlerFunc(server.RandomHandler).HeadersRegexp("visitor", "[0-9]{10,20}")
-
+	r.PathPrefix("/api/videos/random").Methods("GET").HandlerFunc(server.GetRandom).HeadersRegexp("visitor", "[0-9]{10,20}")
+	r.PathPrefix("/api/videos/{video_id}/{reaction:(?:cool|trash)}").Methods("POST").HandlerFunc(server.WriteReaction).HeadersRegexp("visitor", "[0-9]{10,20}")
+	r.PathPrefix("/api/videos/{video_id}").Methods("GET").HandlerFunc(server.GetVideo)
+	
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("frontend/static"))))
 	r.PathPrefix("/").Methods("GET").HandlerFunc(
-		func (w http.ResponseWriter, r *http.Request) {
+		func(w http.ResponseWriter, r *http.Request) {
 			err := templates.ExecuteTemplate(w, "random.html", nil)
 			if err != nil {
 				log.Println(err.Error())
 			}
 		})
+
+	r.Use(loggingMiddleware)
 
 	return server
 }
@@ -86,9 +90,11 @@ func (s *Server) Shutdown(ctx context.Context) error {
 	return nil
 }
 
-func LogRequestedUrl(h http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("new request:", r.URL.Path)
-		h.ServeHTTP(w, r)
-	})
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Do stuff here
+        log.Println(r.RequestURI)
+        // Call the next handler, which can be another middleware in the chain, or the final handler.
+        next.ServeHTTP(w, r)
+    })
 }
