f0d827b23c47ec82316632f42a777d3172acb1ce
thedmdim <dmitruk.dd@tk23.ru>
2024-02-09 16:01:58 +0400

    small changes

--- app/handlers/rating_handler.go
+++ app/handlers/rating_handler.go
@@ -1,12 +1,6 @@
 package handlers
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"zombiezen.com/go/sqlite"
-)
+import "net/http"
 
 type Rating struct {
 	Best  []*RatedCam
@@ -19,75 +13,61 @@ type RatedCam struct {
 	Reactions int64
 }
 
+const ratingLimit int64 = 10
+
 func (s *Router) GetRating(w http.ResponseWriter, r *http.Request) {
 
 	conn := s.db.Get(r.Context())
 	defer s.db.Put(conn)
 
-	var err error
-	rating := &Rating{}
-	rating.Best, err = GetTopRated(conn, true, 10)
-	if err != nil {
-		log.Println("GetTopRated:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	rating.Worst, err = GetTopRated(conn, false, 10)
-	if err != nil {
-		log.Println("GetTopRated:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	templates.ExecuteTemplate(w, "rating.html", rating)
-
-}
-
-func GetTopRated(conn *sqlite.Conn, coolRated bool, limit int64) ([]*RatedCam, error) {
-
-	var cool int64
-	if coolRated {
-		cool = 1
-	}
-
 	stmt := conn.Prep(`
-		SELECT SUM(reactions.cool = ?) AS reactions_sum, reactions.cam_id, videos.title
+		SELECT SUM(reactions.like = ?) AS reactions_sum, reactions.cam_id
 		FROM reactions
-		JOIN videos ON videos.id = reactions.cam_id
+		JOIN cams ON cams.id = reactions.cam_id
 		GROUP BY reactions.cam_id
 		ORDER BY reactions_sum DESC
 		LIMIT ?
 	`)
 
-	stmt.BindInt64(1, cool)
-	stmt.BindInt64(2, limit)
+	stmt.BindInt64(1, 1)
+	stmt.BindInt64(2, ratingLimit)
 
-	var result []*RatedCam
+	rating := &Rating{}
 	for {
 		row, err := stmt.Step()
 		if err != nil {
-			return nil, err
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		if row {
-			ratedVideo := &RatedCam{
+			rating.Best = append(rating.Best, &RatedCam{
 				ID:        stmt.GetText("cam_id"),
-				Title:     stmt.GetText("title"),
 				Reactions: stmt.GetInt64("reactions_sum"),
-			}
-			result = append(result, ratedVideo)
+			})
+		} else { break }
+	}
 
-		} else {
-			break
+	stmt.Reset(); stmt.ClearBindings()
+
+	stmt.BindInt64(1, 0)
+	stmt.BindInt64(2, ratingLimit)
+
+	for {
+		row, err := stmt.Step()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+
+		if row {
+			rating.Worst = append(rating.Worst, &RatedCam{
+				ID:        stmt.GetText("cam_id"),
+				Reactions: stmt.GetInt64("reactions_sum"),
+			})
+		} else { break }
 	}
 
-	if err := stmt.Reset(); err != nil {
-		return nil, fmt.Errorf("stmt.Reset: %w", err)
-	}
-	if err := stmt.ClearBindings(); err != nil {
-		return nil, fmt.Errorf("stmt.ClearBindings: %w", err)
-	}
+	templates.ExecuteTemplate(w, "rating.html", rating)
 
-	return result, nil
 }
